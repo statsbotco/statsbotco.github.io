62794daa5652f69b5f4c40948eac7b1af5a4ad10
statsbotco <nataliya-mikhaylova-1975@mail.ru>
2022-11-30 13:09:45 +0300

    init

---
+++ cubejs/event-analytics/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cubejs/event-analytics/precache-manifest.0805cc346d2f0035e07eebcac9b2230f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cubejs/event-analytics/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
---
+++ cubejs/nginx-analytics-dashboard/precache-manifest.21e3a8e8845c2d4d02aec1be784c1abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fdb1b8e70a8d4ca6bea",
+    "url": "/cubejs/nginx-analytics-dashboard/static/js/runtime~main.fbc69acc.js"
+  },
+  {
+    "revision": "cc7d56f9cd1450dd5fa1",
+    "url": "/cubejs/nginx-analytics-dashboard/static/js/main.a558fd6b.chunk.js"
+  },
+  {
+    "revision": "7ecbb6508610fc22eb68",
+    "url": "/cubejs/nginx-analytics-dashboard/static/js/2.d7ce2031.chunk.js"
+  },
+  {
+    "revision": "cc7d56f9cd1450dd5fa1",
+    "url": "/cubejs/nginx-analytics-dashboard/static/css/main.bbc8dbdd.chunk.css"
+  },
+  {
+    "revision": "7ecbb6508610fc22eb68",
+    "url": "/cubejs/nginx-analytics-dashboard/static/css/2.a47ebc4d.chunk.css"
+  },
+  {
+    "revision": "84c2db412c3df7d8ba7782ce6c3a8afe",
+    "url": "/cubejs/nginx-analytics-dashboard/index.html"
+  }
+];
